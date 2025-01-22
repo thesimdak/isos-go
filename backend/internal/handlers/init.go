e150481a3153d9fb4ec6bf2186ba3065db437f42
martin.simon <martin.simon@onpier.de>
2025-01-22 21:06:29 +0100

    prepare frontend structure

--- backend/internal/handlers/init.go
+++ backend/internal/handlers/init.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"database/sql"
+	"html/template"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/thesimdak/goisos/internal/handlers/upload"
@@ -32,6 +34,75 @@ func Initialize(db *sql.DB) {
 	uploadHandler := upload.NewUploadHandler(competitionService)
 
 	router.POST("/upload", uploadHandler.Upload)
+	// pages
+	router.GET("/", func(c *gin.Context) {
+		renderPage(c, "competition-list.html")
+	})
+	router.GET("/competition-list", func(c *gin.Context) {
+		renderPartial(c, "competition-list.html")
+	})
+	router.GET("/top-results", func(c *gin.Context) {
+		renderPartial(c, "top-results.html")
+	})
+	router.GET("/management", func(c *gin.Context) {
+		renderPartial(c, "management.html")
+	})
 
+	// partials
+	// season dropdown
+	router.GET("/seasons", func(c *gin.Context) {
+		renderPartial(c, "seasons.html")
+	})
+
+	router.GET("/season-list", func(c *gin.Context) {
+		renderPage(c, "seasons.html")
+	})
+	// Load templates
+	router.SetHTMLTemplate(parseTemplates())
+
+	// Dynamic competition list route
+	router.GET("/competitions", func(c *gin.Context) {
+		var competitions []string
+		competitions = append(competitions, "Memorial Bedricha Supcika")
+		competitions = append(competitions, "Mistrovstvi Ceske Republiky")
+
+		c.HTML(http.StatusOK, "competitions.html", gin.H{
+			"Competitions": competitions,
+		})
+	})
 	router.Run(":8080")
+}
+
+func renderPage(c *gin.Context, templateName string) {
+	c.HTML(http.StatusOK, "layout.html", gin.H{
+		"contentTemplate": templateName,
+	})
+}
+
+func renderPartial(c *gin.Context, templateName string) {
+	if c.GetHeader("HX-Request") == "true" {
+		c.HTML(http.StatusOK, templateName, nil)
+		return
+	}
+	renderPage(c, templateName)
+}
+
+func renderComponent(c *gin.Context, templateName string) {
+	if c.GetHeader("HX-Request") == "true" {
+		c.HTML(http.StatusOK, templateName, nil)
+		return
+	}
+}
+
+// Parse templates from the main directory and components subdirectory
+func parseTemplates() *template.Template {
+	tmpl := template.New("") // Create a new template instance
+
+	// Parse templates from the main directory
+	tmpl = template.Must(tmpl.ParseGlob("templates/*.html"))
+
+	// Parse templates from the components subdirectory
+	tmpl = template.Must(tmpl.ParseGlob("templates/components/*.html"))
+
+	return tmpl
 }
---
+++ backend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.{html,js}', // Path to your HTML files
+    './static/js/**/*.{js,ts}',  // Path to your JS files (optional)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
